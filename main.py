02e5fcb92000bb00242afc7f95319e10e8e72382
ayeshamk <ayesha.malika.khan@gmail.com>
2023-01-11 21:31:13 +0500

    Created Loan Summary API

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-
 import uvicorn
 from typing import List
 import json
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException,Request
 from sqlalchemy.orm import Session
 
-from sql_loan import schemas, user_functions, models,loan_function
+from sql_loan import schemas, user_functions, models, loan_function
 from sql_loan.database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
@@ -36,17 +35,24 @@ def read_users(db: Session = Depends(get_db)):
     return users
 
 
-
 @app.post("/users/{user_id}/loans/", response_model=schemas.Loan)
 def create_loan_for_user(
         user_id: int, loan: schemas.LoanCreate, db: Session = Depends(get_db)):
     return loan_function.create_loan(db=db, loan=loan, user_id=user_id)
 
 
+@app.post("/loan_summary/")
+async def get_loan_summary(request:Request, db: Session = Depends(get_db)):
+    json_data = await request.json()
+    summary = loan_function.get_loan_summary(db, json_data)
+    return summary
+
+
 @app.get("/loans/{user_id}", response_model=List[schemas.Loan])
-def read_loan(user_id:int,db: Session = Depends(get_db)):
+def read_loan(user_id: int, db: Session = Depends(get_db)):
     loan = loan_function.get_loans(db, user_id)
     return loan
+
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8000)
--- sql_loan/loan_function.py
+++ sql_loan/loan_function.py
@@ -1,11 +1,28 @@
 from sqlalchemy.orm import Session
-
-from . import models,schemas
+import numpy as np
+from . import models, schemas
 from fastapi import HTTPException
 
 
 def get_loans(db: Session, user_id: int):
     return db.query(models.Loan).filter(models.Loan.owner_id == user_id).all()
+
+
+def get_loan_summary(db: Session, json_data):
+    loan_record = db.query(models.Loan).filter(models.Loan.owner_id == json_data.get("user_id")).all()[
+        (json_data.get('loan_id') - 1)]
+    total_amont = loan_record.amount
+    interest_rate = loan_record.annualInterestRate / 100
+    interest_rate_per_month = interest_rate / 12
+    loanTerm_per_year = (loan_record.loanTerm / 12) * 12
+    new_item = dict()
+    new_item['Principal_Balance'] = abs(
+        np.ppmt(interest_rate_per_month, json_data.get('month'), loanTerm_per_year, total_amont))
+    new_item['interest_already_paid'] = abs(np.cumsum(
+        np.ipmt(interest_rate_per_month, range(1, json_data.get('month') + 1), loanTerm_per_year, total_amont))[-1])
+    new_item['principal_amount_already_paid'] = abs(np.cumsum(
+        np.ppmt(interest_rate_per_month, range(1, json_data.get('month') + 1), loanTerm_per_year, total_amont))[-1])
+    return new_item
 
 
 def create_loan(db: Session, loan: schemas.LoanCreate, user_id: int):
